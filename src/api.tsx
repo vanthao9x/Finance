b28f172396ffb1d25c0caad281ead61dec43a65c
vanthao9x <levanthaolvt228@gmail.com>
2025-05-12 17:00:44 +0700

    get company info and build dash board

--- src/api.tsx
+++ src/api.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { CompanySearch } from "./company";
+import { CompanyProfile, CompanySearch } from "./company";
 
 interface searchResponse {
   data: CompanySearch[];
@@ -9,6 +9,22 @@ export const searchCompanies = async (query: string) => {
   try {
     const response = await axios.get<searchResponse>(
       `https://financialmodelingprep.com/api/v3/search-name?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      console.error('Axios error:', error.message);
+    } else {
+      console.error('Unknown error:', error);
+    }
+    return null; // 👈 kiểu nhất quán
+  }
+};
+
+export const getCompanyProfile = async (query: string) => {
+  try {
+    const response = await axios.get<CompanyProfile[]>(
+      `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
     );
     return response.data;
   } catch (error: unknown) {
--- src/pages/CompanyPage/CompanyPage.tsx
+++ src/pages/CompanyPage/CompanyPage.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CompanyProfile } from "../../company";
+import { getCompanyProfile } from "../../api";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import CompanyDashboard from "../../components/CompanyDashboard/CompanyDashboard";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
-  return (
-    <div>CompanyPage</div>
-  )
-}
+  let { ticker } = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
 
-export default CompanyPage
+  useEffect(() => {
+    const getProfile = async () => {
+      const result = await getCompanyProfile(ticker!);
+      if (Array.isArray(result)) {
+        setCompany(result[0]);
+        console.log("company: dsfdsfdsf: ", result[0]);
+      }
+    };
+    getProfile();
+  }, [ticker]);
+  return (
+    <>
+      {company ? (
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <Sidebar  />
+          <CompanyDashboard />
+        </div>
+      ) : (
+        <div>Company not found</div>
+      )}
+    </>
+  );
+};
+
+export default CompanyPage;
